6169dbffafa097ea19af32cadcc122d56245c47e
nimesh44 <nimesh.neupane43@gmail.com>
2020-11-27 07:13:56 +0545

    Adding string_reverse.py file

---
+++ string_reverse.py
@@ -0,0 +1,13 @@
+def revert_string(user_string):
+    user_string = user_string[::-1]
+    print(user_string)
+
+
+
+
+def main():
+    revert_string('Netra')
+
+
+if __name__ == "__main__":
+    main()
